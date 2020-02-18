b900286bce71c01618184f97ce87ae6426817a44
rikiod <54606933+rikiod@users.noreply.github.com>
2020-02-18 21:39:36 +0900

    Update GraphsPyPlot.py

--- GraphsPyPlot.py
+++ GraphsPyPlot.py
@@ -7,29 +7,29 @@
 import matplotlib.pyplot as pyplot
 import random
 
-# graphing 1000 random numbers between 1 and 100
+# graphing 1000 random numbers between 1 and 1000 (#1)
 x = [i for i in range(1, 1000)]
 y = [random.randint(1, 100) for i in x] # same as appending
 
-#calculating the average of the 1000 random numbers using a loop
+# graphing the numbers 
+pyplot.plot(x, y)
+pyplot.xlabel('x')
+pyplot.ylabel('y')
+pyplot.show()
+
+#calculating the average of the 1000 random numbers using a loop (#2)
 sum = 0
 for num in y:
     sum = sum + num
 avg = sum / 1000
 print (avg)
 
-# graphing the numbers
-pyplot.plot(x, y)
-pyplot.xlabel('x')
-pyplot.ylabel('y')
-pyplot.show()
-
-# graphing the sine function
+# graphing the sine function (#3)
 x = np.arange(-10, 10, 0.1) # setting range from -10 to 10 with steps of 0.1
 y = np.sin(0.5 * x) * 14
 pyplot.show()
 
-# graphing a
+# graphing a circle (#4)
 fig, ax = pyplot.subplots() # defining a set of axes
 ax.set(xlim=(-10, 10), ylim = (-10, 10)) # setting window of axes
 
