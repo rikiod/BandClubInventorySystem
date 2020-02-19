3c7a7ea197a056632480dd02cbac167e4b63aa40
rikiod <54606933+rikiod@users.noreply.github.com>
2020-02-19 20:25:33 +0900

    Update GraphsPyPlot.py
    added alternative way to plot sine as learned in class

--- GraphsPyPlot.py
+++ GraphsPyPlot.py
@@ -3,38 +3,42 @@
 # ②　 Calculate the average of the 1000 random numbers in ① using a loop.
 # ③ 　Graph the equation for the wave function 14*sin(0.5*x) in the range x = [-10, 10] with steps of 0.1.
 # ④ 　Create the graph on any other mathematical function you know.
+import math
 
 import matplotlib.pyplot as pyplot
 import random
 
-# graphing 1000 random numbers between 1 and 1000 (#1)
+# graphing 1000 random numbers between 1 and 100
 x = [i for i in range(1, 1000)]
 y = [random.randint(1, 100) for i in x] # same as appending
 
-# graphing the numbers 
+# calculating the average of the 1000 random numbers using a loop
+sum = 0
+for num in y:
+    sum = sum + num
+avg = sum / 1000
+print(avg)
+
+# alternative way to create steps without using numpy library
+x = []
+y = [] # y = 14sin(0.5x)
+minimum = -10
+for i in range(200):
+    x.append(minimum + 0.1 * (i + 1))
+    y.append(14 * math.sin(0.5 * x[i]))
+
+# graphing the numbers
 pyplot.plot(x, y)
 pyplot.xlabel('x')
 pyplot.ylabel('y')
 pyplot.show()
 
-#calculating the average of the 1000 random numbers using a loop (#2)
-sum = 0
-for num in y:
-    sum = sum + num
-avg = sum / 1000
-print (avg)
-
-# graphing the sine function (#3)
+# graphing the sine function
 x = np.arange(-10, 10, 0.1) # setting range from -10 to 10 with steps of 0.1
 y = np.sin(0.5 * x) * 14
 pyplot.show()
 
-# alternative way to create steps without using numpy library
-# x = [] 
-# for x in range(2000):
-#     x.append(min + 0.01(i+1))
-
-# graphing a circle (#4)
+# graphing a circle
 fig, ax = pyplot.subplots() # defining a set of axes
 ax.set(xlim=(-10, 10), ylim = (-10, 10)) # setting window of axes
 
