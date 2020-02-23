249e3cd6cf627735467e6a2737d4a85052cf14f1
rikiod <54606933+rikiod@users.noreply.github.com>
2020-02-23 17:45:41 +0900

    Delete 2_evenElements.py

--- listPractice/2_evenElements.py
+++
@@ -1,9 +0,0 @@
-# Given a list of numbers, find and print all elements that are an even number. In this case use a for-loop that
-# iterates over the list, and not over its indices! That is, don't use range()
-
-numbers = input().split()
-for i in range(len(numbers)):
-    numbers[i] = int(numbers[i])
-for element in numbers:
-    if element % 2 == 0:
-        print (element)
