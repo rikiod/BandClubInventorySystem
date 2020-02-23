65a5f0a9cfd6e733b8ca8429b119de946d577511
rikiod <54606933+rikiod@users.noreply.github.com>
2020-02-23 17:46:05 +0900

    Delete 4_neighborsOfTheSameSign.py

--- listPractice/4_neighborsOfTheSameSign.py
+++
@@ -1,8 +0,0 @@
-# Given a list of numbers, find and print the first adjacent elements which have the same sign. If there is no such
-# pair, leave the output blank.
-
-numbers = [int(i) for i in input().split()]
-for i in range (1, len(numbers)):
-    if numbers[i] * numbers[i-1] > 0:
-        print (numbers[i-1], numbers[i])
-        break
