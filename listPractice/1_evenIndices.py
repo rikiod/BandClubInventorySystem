f02f726f271956fff07624de56d2926b876afbe6
rikiod <54606933+rikiod@users.noreply.github.com>
2020-02-23 17:45:32 +0900

    Delete 1_evenIndices.py

--- listPractice/1_evenIndices.py
+++
@@ -1,5 +0,0 @@
-# Given a list of numbers, find and print all the list elements with an even index number.
-
-numbers = input().split() # receiving input and putting into list of strings (reason for last of int when putting in input)
-for i in range(0, len(numbers), 2): # step of 2 in order to get only even numbers
-    print(numbers[i])
